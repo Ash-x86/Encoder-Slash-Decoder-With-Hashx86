d4b3b7f23ef5ba65cd3c7f362c0536cb443b0b61
Ash_x128 <ashbutdummer@gmail.com>
2023-01-03 19:17:05 +0530

    Add project files.

---
+++ Encoder-Decoder/Encoder.py
@@ -0,0 +1,74 @@
+
+def Code(char):
+    if char in ["A","a"]:
+        return '1'
+    elif char in ["B","b"]:
+        return '2'
+    elif char in ["C","c"]:
+        return "3"
+    elif char in ["D","d"]:
+        return "4"
+    elif char in ["E","e"]:
+        return "5"
+    elif char in ["F","f"]:
+        return "6"
+    elif char in ["G","g"]:
+        return "7"
+    elif char in ["H","H"]:
+        return "8"
+    elif char in ["I","i"]:
+        return "9"
+    elif char in ["J","j"]:
+        return "!"
+    elif char in ["K","K"]:
+        return "@"
+    elif char in ["L","l"]:
+        return "#"
+    elif char in ["M","m"]:
+        return "$"
+    elif char in ["N","n"]:
+        return "%"
+    elif char in ["O","o"]:
+        return "^"
+    elif char in ["P","p"]:
+        return "&"
+    elif char in ["Q","q"]:
+        return "*"
+    elif char in ["R","r"]:
+        return "("
+    elif char in ["S","s"]:
+        return ")"
+    elif char in ["T","t"]:
+        return "-"
+    elif char in ["U","u"]:
+        return "_"
+    elif char in ["V","v"]:
+        return "="
+    elif char in ["W","w"]:
+        return "+"
+    elif char in ["X","x"]:
+        return "{"
+    elif char in ["Y","y"]:
+        return "}"
+    elif char in ["Z","z"]:
+        return "\"
+
+    # Alphabets complete
+    # Numbers Start
+
+    elif char in [1,"1"]:
+        return "A"
+    elif char in [2,"2"]:
+        return "B"
+    elif char in [3,"3"]:
+        return "C"
+    elif char in [4,"4"]:
+        return "D"
+    elif char in [5,"5"]:
+        return "E"
+    elif char in [6,"6"]:
+        return "F"
+    elif char in [7,"7"]:
+        return "G"
+    elif char in [8,"8"]:
+        return "H"
