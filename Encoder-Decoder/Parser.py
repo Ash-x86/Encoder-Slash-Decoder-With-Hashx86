a1f47ba34e331a9879959d115c2c699652d97ab6
Ash_x64 <ashbutdummer+1@gmail.com>
2023-01-03 19:55:41 +0530

    Fixed Issues

--- Encoder-Decoder/Parser.py
+++ Encoder-Decoder/Parser.py
@@ -1,4 +1,10 @@
+import code
 import Encoder as E
 
 print("This is a Encoder/Decoder to a Hashing Algorithm like base64 by Ash_x86")
-input("> ")
+string = input("> ")
+cc = "";
+for i in range(len(string)):
+    c = str(string[i:len(string)])
+    cc = cc+str(E.Code(c))
+print(cc)
